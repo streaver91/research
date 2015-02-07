16455a29022b8fee947aeac066922a30e631c601
Junhao Li <streaver91@gmail.com>
2015-02-07 16:53:35 -0500

    Signed-off-by: Junhao Li <streaver91@gmail.com>

--- TickSize/reports.node.js
+++ TickSize/reports.node.js
@@ -1,9 +1,125 @@
-var xls = require('xlsjs');
+var xlsjs = require('xlsjs');
 var fs = require('fs');
+var async = require('async');
 
-var workbook = xls.readFile('Reports/special/BE0003008019.xls');
+var LIQ_RATIO_FILE = 'liqRatio.txt';
+var EFFECTIVE_TICK_FILE = 'effectiveTick.txt';
+var LIST_FILE = 'list.txt';
+var PANEL_FILE = 'panel.txt';
+var LIQ_RATIO_FILE_STRUCTURE = ['ISIN', 'MONTH', 'LIQ_RATIO', 'TREAT'];
+var EFFECTIVE_TICK_FILE_STRUCTURE = ['ISIN', 'MONTH', 'EFFECTIVE_TICK', 'TREAT', 'AVG_PRICE'];
+var LIST_FILE_STRUCTURE = ['ISIN', 'NAME', 'GROUP'];
 
-var sheetNameList = workbook.SheetNames;
-var sheet1A1 = workbook.Sheets[sheetNameList[0]]['A1'].v;
+var liqRatio = [];
+var effectiveTick = [];
+var treat = [];
+var avgPrice = [];
+var isinList = [];
+var reportItemList = [];
+var reportData = [];
 
-console.log(sheetNameList);
+var getLiquidity = function(callback) {
+  console.log('GETTING LIQUIDITY');
+  var _getLiqRatio = function(callback) {
+    fs.readFile(LIQ_RATIO_FILE, 'utf-8', function(err, res) {
+      res = res.split(/[\r]?\n/g);
+      for(var i = 0; i < res.length; i++) {
+        var curRes = res[i].split(/\t/g);
+        var curIsin = curRes[LIQ_RATIO_FILE_STRUCTURE.indexOf('ISIN')];
+        var curMonth = curRes[LIQ_RATIO_FILE_STRUCTURE.indexOf('MONTH')];
+        var curLiqRatio = curRes[LIQ_RATIO_FILE_STRUCTURE.indexOf('LIQ_RATIO')];
+        liqRatio[curMonth + '_' + curIsin] = curLiqRatio;
+      }
+      callback(null);
+    });
+  };
+  
+  var _getEffectiveTick = function(callback) {
+    fs.readFile(EFFECTIVE_TICK_FILE, 'utf-8', function(err, res) {
+      res = res.split(/[\r]?\n/g);
+      for(var i = 0; i < res.length; i++) {
+        var curRes = res[i].split(/\t/g);
+        var curIsin = curRes[LIQ_RATIO_FILE_STRUCTURE.indexOf('ISIN')];
+        var curMonth = curRes[LIQ_RATIO_FILE_STRUCTURE.indexOf('MONTH')];
+        var curEffectiveTick = curRes[LIQ_RATIO_FILE_STRUCTURE.indexOf('EFFECTIVE_TICK')];
+        effectiveTick[curMonth + '_' + curIsin] = curEffectiveTick;
+      }
+      callback(null);
+    });
+  };
+  
+  var _getTreatInfo = function(callback) {
+    fs.readFile(EFFECTIVE_TICK_FILE, 'utf-8', function(err, res) {
+      res = res.split(/[\r]?\n/g);
+      for(var i = 0; i < res.length; i++) {
+        var curRes = res[i].split(/\t/g);
+        var curIsin = curRes[EFFECTIVE_TICK_FILE_STRUCTURE.indexOf('ISIN')];
+        var curMonth = curRes[EFFECTIVE_TICK_FILE_STRUCTURE.indexOf('MONTH')];
+        var curTreat = curRes[EFFECTIVE_TICK_FILE_STRUCTURE.indexOf('TREAT')];
+        var curAvgPrice = curRes[EFFECTIVE_TICK_FILE_STRUCTURE.indexOf('AVG_PRICE')];
+        treat[curMonth + curIsin] = curTreat;
+        avgPrice[curMonth + '_' + curIsin] = curAvgPrice;
+      }
+      callback(null);
+    });
+  };
+  
+  async.parallel([
+    _getLiqRatio,
+    _getEffectiveTick,
+    _getTreatInfo
+  ], function(err, res) {
+    callback(null);
+  });
+};
+
+var getReports = function(callback) {
+  console.log('GETTING REPORT');
+  
+  var _getIsinList = function(callback) {
+    fs.readFile(LIST_FILE, 'utf-8', function(err, res) {
+      res = res.split(/[\r]?\n/g);
+      for(var i = 0; i < res.length; i++) {
+        var curRes = res[i].split(/\t/g);
+        isinList.push(curRes[LIST_FILE_STRUCTURE.indexOf('ISIN')]);
+      }
+      callback(null);
+    });
+  };
+  
+  var _getReports = function(callback) {
+    for(var i = 0; i < isinList.length; i++) {
+      var curIsin = isinList[i];
+      try {
+        var workbook = xlsjs.readFile(__dirname + '\\Reports\\' + curIsin + '.xls');
+        console.log(workbook.SheetNames);
+        
+      } catch (err) {
+        console.log('NOT FOUND: ' + curIsin);
+      }
+    }
+    callback(null);
+  };
+  
+  async.series([
+    _getIsinList,
+    _getReports
+  ], function() {
+    callback(null);
+  });
+};
+
+var genPanel = function(callback) {
+  console.log('GENERATING PANEL');
+  callback(null);
+};
+
+async.series([
+  getLiquidity,
+  getReports,
+  genPanel
+], function() {
+  console.log('DONE');
+  console.log('DATA SAVED TO: ' + PANEL_FILE);
+  debugger;
+});
